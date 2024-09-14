bb016dbb7ac5b7b4f50502b4de605efde6234b90
Sadia Nawaaz <sadianawaaz1995@gmail.com>
2024-09-14 11:03:02 +0500

    Added For Testing
    Added For Testing

---
+++ UITests.Shared/MainPageTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+
+// You will have to make sure that all the namespaces match
+// between the different platform specific projects and the shared
+// code files. This has to do with how we initialize the AppiumDriver
+// through the AppiumSetup.cs files and NUnit SetUpFixture attributes.
+// Also see: https://docs.nunit.org/articles/nunit/writing-tests/attributes/setupfixture.html
+namespace UITests;
+
+// This is an example of tests that do not need anything platform specific
+public class MainPageTests : BaseTest
+{
+	[Test]
+	public void AppLaunches()
+	{
+		App.GetScreenshot().SaveAsFile($"{nameof(AppLaunches)}.png");
+	}
+
+	[Test]
+	public void ClickCounterTest1()
+	{
+		
+            // Arrange
+            // Find elements with the value of the AutomationId property
+            Task.Delay(2000).Wait(); // Wait for 2 seconds before clicking
+
+            var element = FindUIElement("CounterBtn");
+
+            // Act
+            element.Click();
+            Task.Delay(500).Wait(); // Wait for the click to register and show up on the screenshot
+
+        // Assert
+        string dateTimeNow = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+        // Save the screenshot with DateTime included in the filename
+        App.GetScreenshot().SaveAsFile($"{nameof(ClickCounterTest)}_{dateTimeNow}.png");
+
+
+        Console.WriteLine("i m runinng 12");
+        Assert.That(element.Text, Is.EqualTo("Clicked 1 time"));
+    
+	}
+
+
+    [Test]
+    public void ClickCounterWelcomeTest()
+        {
+     
+            // Arrange
+            Task.Delay(2000).Wait(); 
+
+            var element = FindUIElement("Welcome");
+        string dateTimeNow = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+        // Save the screenshot with DateTime included in the filename
+        App.GetScreenshot().SaveAsFile($"{nameof(ClickCounterTest)}_{dateTimeNow}.png");
+        Assert.That(element.Text,Is.EqualTo("Welcome"));
+
+
+        Console.WriteLine("i m runinng 566");
+
+
+        }
+
+    [Test]
+    public void ClickCounterTest()
+        {
+        // Get the root path of the application dynamically
+        string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+        string logFilePath = Path.Combine(rootPath, "TestLogs", "TestLog.txt"); // Create the file in a 'TestLogs' directory
+
+        // Ensure that the directory exists
+        Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
+
+        string testName = nameof(ClickCounterTest);
+
+            // Arrange
+            Task.Delay(2000).Wait(); // Wait for 2 seconds before clicking
+            var element = FindUIElement("CounterBtn");
+
+            // Act
+            element.Click();
+            Task.Delay(500).Wait(); // Wait for the click to register and show up on the screenshot
+
+            // Assert
+            string dateTimeNow = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+            // Save the screenshot with DateTime included in the filename
+            App.GetScreenshot().SaveAsFile(Path.Combine(rootPath, $"{testName}_{dateTimeNow}.png"));
+
+            // Assert element text
+            Assert.That(element.Text, Is.EqualTo("Clicked 1 time"));
+
+        // Log success to the text file
+
+        Console.WriteLine("i m runinng 3333");
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                {
+                writer.WriteLine($"{DateTime.Now}: Test '{testName}' passed.");
+                }
+            
+      
+        }
+
+
+    }
---
+++ UITests.Windows/AppiumSetup.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace UITests;
+
+[SetUpFixture]
+public class AppiumSetup
+{
+	private static AppiumDriver? driver;
+
+	public static AppiumDriver App => driver ?? throw new NullReferenceException("AppiumDriver is null");
+
+	[OneTimeSetUp]
+	public void RunBeforeAnyTests()
+	{
+		// If you started an Appium server manually, make sure to comment out the next line
+		// This line starts a local Appium server for you as part of the test run
+		AppiumServerHelper.StartAppiumLocalServer();
+
+        //var windowsOptions = new AppiumOptions
+        //{
+        //	// Specify windows as the driver, typically don't need to change this
+        //	AutomationName = "windows",
+        //	// Always Windows for Windows
+        //	PlatformName = "Windows",
+        //          // The identifier of the deployed application to test
+        //          //App = "com.companyname.basicappiumsample_9zz4h110yvjzm!App",
+        //          App = @"F:\YamlsTestingProject\YamlTesting\Yaml\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\Yaml.exe"
+
+        //          };
+        //var currentDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        //// Use relative path based on the current directory
+        //var appPath = Path.Combine(currentDir, @"..\..\..\Yaml\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\Yaml.exe");
+
+        //var windowsOptions = new AppiumOptions
+        //    {
+        //    AutomationName = "windows",
+        //    PlatformName = "Windows",
+        //    App = appPath  // Use dynamically calculated path
+        //    };
+        //string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Yaml\\bin\\Debug\\net8.0-windows10.0.19041.0\\win10-x64\\Yaml.exe"));
+        /// string appPath ="https://github.com/sadial217428/YamlTesting/blob/main/Yaml/bin/Debug/net8.0-windows10.0.19041.0/win10-x64/Yaml.exe";
+
+
+        /// string projectRoot = @"Yaml\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\Yaml.exe";
+
+        //string appPath =Path.Combine(projectRoot, @"YamlsTestingProject\YamlTesting\Yaml\bin\Debug\net8.0-windows10.0.19041.0\win10-x64\Yaml.exe");
+
+        // Log the path for debugging purposes
+
+        string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", ".."));
+
+        // Combine the root directory with the relative path to the executable
+        string appPath = Path.Combine(projectRoot, "Yaml", "bin", "Debug", "net8.0-windows10.0.19041.0", "win10-x64", "Yaml.exe");
+
+
+        Console.WriteLine($"Application Path: {appPath}");
+
+        var windowsOptions = new AppiumOptions
+            {
+            AutomationName = "windows",
+            PlatformName = "Windows",
+            App = appPath  // Use dynamically calculated path
+            };
+
+        // Note there are many more options that you can use to influence the app under test according to your needs
+
+        driver = new WindowsDriver(windowsOptions);
+	}
+
+	[OneTimeTearDown]
+	public void RunAfterAnyTests()
+	{
+		driver?.Quit();
+
+		// If an Appium server was started locally above, make sure we clean it up here
+		AppiumServerHelper.DisposeAppiumLocalServer();
+	}
+}
